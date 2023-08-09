ad964140dc80d58f78f94bd9ed7a44a69f836388
Nguyễn Tuấn Tiến <strikezz2003@gmail.com>
2023-08-09 13:50:27 +0700

    Object Movement C# unity

---
+++ CubeMovement01.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement01 : MonoBehaviour
+{
+    public float speed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 100;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //di chuyển object theo hướng mũi tên 
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }else
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }else
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+        }else
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * speed);
+        }
+    }
+}
